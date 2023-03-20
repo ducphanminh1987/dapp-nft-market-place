28958f27895115413f343f5979a844e4d3de5e9d
ducoceannetsoft <ducphan@oceannetsoft.com>
2023-03-20 17:33:59 +0700

    hardhat config for ganache

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -11,5 +11,11 @@ module.exports = {
         "5b0e77b89722227d8fd989901c520dfa01952d7afb4b82f1318dd26dc9bf1ded",
       ],
     },
+    ganache: {
+      url: "HTTP://127.0.0.1:7545",
+      accounts: [
+        "a29934b1621d559669e4c49c7fe84ce8db16f1331b7ea2cd190a9cd3b66c9985",
+      ],
+    },
   },
 };
